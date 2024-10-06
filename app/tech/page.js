842be907cbfe93e1aed8670c4329894476cb2d2c
xalc <chao1990li@gmail.com>
2024-10-06 18:01:25 +0800

    tensor test on tech page

--- app/tech/page.js
+++ app/tech/page.js
@@ -1,11 +1,14 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react';
+import TfvisCore from './tfvis-react';
+import { Button } from 'antd';
+import styles from './tech.modules.scss';
 const TechPage = () => {
 
-
-    return (<>
+    return (<div className={styles.container}>
         <h2>Tech page</h2>
-    </>);
+        <TfvisCore ></TfvisCore>
+    </div>);
 }
 export default TechPage;
