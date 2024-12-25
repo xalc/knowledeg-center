55e8372957de0744c977cb5e17890be78fa8c211
xalc <chao1990li@gmail.com>
2024-12-25 16:26:07 +0800

    validate cache

--- src/app/about/@wereader/page.js
+++ src/app/about/@wereader/page.js
@@ -1,18 +1,24 @@
 import WRBooks from '@/components/wereaderBooks';
-
+import { unstable_cache } from 'next/cache'
 import { getDBReadingBooks, getAllReadingStatus } from '@/libs/db-utils';
 export const revalidate = 60 * 60;
+
+
+const getBooks = unstable_cache(async () => await getDBReadingBooks().catch(err => {
+	//TODO Error handle
+	console.error('get book list failed: ' + err);
+}), ['posts'],
+	{ revalidate: 3600, tags: ['posts'] });
+const getBookStatus = unstable_cache(async () => await getAllReadingStatus().catch(err => {
+	//TODO Error handle
+	console.error('get book list failed: ' + err);
+}), ['posts'],
+	{ revalidate: 3600, tags: ['posts'] });
 const WeReaderPage = async () => {
 	// const dbInstance = DBInstance.getInstance('knowledge', 'books');
 
-	const books = await getDBReadingBooks().catch(err => {
-		//TODO Error handle
-		console.error('get book list failed: ' + err);
-	});
-	const statuses = await getAllReadingStatus().catch(err => {
-		//TODO Error handle
-		console.error('get book list failed: ' + err);
-	});
+	const books = await getBooks();
+	const statuses = await getBookStatus();
 
 	const needed = books.map(b => {
 		return {
