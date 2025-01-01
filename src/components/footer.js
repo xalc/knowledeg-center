6cb8baedda6ecf3302e9a8096767556e45e07570
xalc <chao1990li@gmail.com>
2025-01-01 10:59:27 +0800

    footer implovement

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+
+export default function FooterContent() {
+  return (
+    <div>
+
+      <p><a
+        href="mailto:huntxalc@gmail.com"
+      >
+        HunterX
+      </a> © 	陕ICP备2024057216号-1</p>
+    </div>);
+}
+
